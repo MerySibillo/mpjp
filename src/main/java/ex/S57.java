7a5932638e142fed2134feb20d807dc51007b18a
Mariarosaria Sibillo <mariarosaria.sibillo@gmail.com>
2019-12-16 17:43:28 +0100

    modifiche

--- src/main/java/ex/S57.java
+++ src/main/java/ex/S57.java
@@ -1,54 +1,63 @@
 package ex;
 
 public class S57 {
-    /**
-     * Average speed
-     * 
-     * @param distance in meters
-     * @param time     in seconds
-     * @return speed in meters per second
-     */
-    public static double speed(double distance, double time) {
-        // TODO
-        return 0;
-    }
+	/**
+	 * Average speed
+	 * 
+	 * @param distance in meters
+	 * @param time     in seconds
+	 * @return speed in meters per second
+	 */
+	public static double speed(double distance, double time) {
+		return distance / time;
+	}
 
-    /**
-     * Distance between (x0, y0) and (x1, y1)
-     * 
-     * @param x0 first point x
-     * @param y0 first point y
-     * @param x1 second point x
-     * @param y1 second point y
-     * @return distance
-     */
-    public static double distance(int x0, int y0, int x1, int y1) {
-        // TODO
-        return 0;
-    }
+	/**
+	 * Distance between (x0, y0) and (x1, y1)
+	 * 
+	 * @param x0 first point x
+	 * @param y0 first point y
+	 * @param x1 second point x
+	 * @param y1 second point y
+	 * @return distance
+	 */
+	public static double distance(int x0, int y0, int x1, int y1) {
 
-    /**
-     * Engine capacity
-     * 
-     * @param bore in mm
-     * @param stroke in mm
-     * @param nr     number of cylinders
-     * @return the engine capacity in cm^3
-     */
-    public static double engineCapacity(double bore, double stroke, int nr) {
-        // TODO
-        return 0;
-    }
+		var a = x1 - x0;
+		var b = y1 - y0;
+		return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+	}
 
-    // somma delle cifre in un numero
-    /**
-     * Add up all the digits in an integer
-     * 
-     * @param value
-     * @return sum of digits
-     */
-    public static int digitSum(int value) {
-        // TODO
-        return 0;
-    }
+	/**
+	 * Engine capacity
+	 * 
+	 * @param bore   in mm
+	 * @param stroke in mm
+	 * @param nr     number of cylinders
+	 * @return the engine capacity in cm^3
+	 */
+	public static double engineCapacity(double bore, double stroke, int nr) {
+
+		var a = Math.pow(bore / 2, 2);
+		var b = a * Math.PI * stroke * nr;
+		return b / 1000;
+	}
+
+	// somma delle cifre in un numero
+	/**
+	 * Add up all the digits in an integer
+	 * 
+	 * @param value
+	 * @return sum of digits
+	 */
+	public static int digitSum(int value) {
+		int counter = 0;
+		value = Math.abs(value);
+
+		while (value != 0) {
+			counter += value % 10;
+			value /= 10;
+		}
+		return counter;
+	}
 }
---
+++ src/main/java/s049/MyString.java
@@ -0,0 +1,57 @@
+package s049;
+
+public class MyString {
+	static int compareTo(String s, String t) {
+		int lengthS = s.length();
+		int lengthT = t.length();
+		int minLength = 0;
+
+		if (lengthS < lengthT) {
+			minLength = lengthS;
+		} else {
+			minLength = lengthT;
+		}
+
+		for (int i = 0; i < minLength; i++) {
+			if (s.charAt(i) != t.charAt(i)) {
+				return -1;
+			} else if (s.charAt(i) > t.charAt(i)) {
+				return +1;
+			}
+		}
+
+		if (lengthS == lengthT) {
+			return 0;
+		} else if (lengthS < lengthT) {
+			return -1;
+		} else {
+			return +1;
+		}
+	}
+
+	public static void main(String[] args) {
+		String s1 = "hello";
+		String t1 = "world";
+
+		System.out.println(s1.length());
+		System.out.println(t1.length());
+
+		int result = compareTo(s1, t1);
+
+		System.out.println("hello vs world, result is:" + result);
+
+		String s2 = "hello";
+		String t2 = "hello";
+
+		int result2 = compareTo(s2, t2);
+
+		System.out.println("hello vs world, result is:" + result2);
+
+		String s3 = "hello";
+		String t3 = "hell";
+
+		int result3 = compareTo(s3, t3);
+
+		System.out.println("hello vs world, result is" + result3);
+	}
+}
